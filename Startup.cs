20d7d2f16e7181c65412071336873ed1d19040ab
Eric Day <eric.ch.day@gmail.com>
2020-08-04 21:25:13 -0600

    cors fix

--- Startup.cs
+++ Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Http.Cors;
+// using System.Web.Http.Cors;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,18 +27,19 @@ namespace rover
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.AddCors(options =>
             {
-                options.AddPolicy(name: "MyPolicy",
-                                  builder =>
-                                  {
-                                      builder.WithOrigins("https://localhost:5003",
-                                              "http://localhost:5002")
-                                        .WithMethods("PUT", "DELETE", "GET", "POST");
-                                  });
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("localhost")
+                                            .AllowAnyOrigin()
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
             });
 
+            services.AddControllers();
             services.AddSingleton<IRoverRepository, RoverRepository>();
         }
 
